642bfa6b8ef7342d2f70ce1bc817b399e78b170a
Alexandro9911 <31696942+Alexandro9911@users.noreply.github.com>
2017-10-25 23:39:49 +0300

    fixessss+ еще исправления по отрезкам

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -115,9 +115,9 @@ fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
     val ad = d - a
     val cb = b - c
    return when {
-        (b >= c) && (c > a) && (d > b) -> cb
+       (b >= c) && (c > a) && (d >= b) -> cb
         (d >= a) && (a > c) && (d <= b) -> ad
-        (a <= c) && (d < b) && (a < d) -> cd
+       (a <= c) && (d < b) && (a <= d) -> cd
         (c <= a) && (b < d) -> ab
         (a == c) &&  (b == d) -> ab
  else -> -1
